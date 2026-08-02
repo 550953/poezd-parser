c13a4b33ebf9f381a5035d3ba065a4c645f47f77
Provodnik Deploy <deploy@provodnik.xyz>
2026-08-02 20:02:31 +0300

    Add Sentry SDK integration for error monitoring and logging

--- api/connection.sample.php
+++ api/connection.sample.php
@@ -2,6 +2,14 @@
 // Скопируй этот файл в connection.php и заполни реальными значениями
 // cp connection.sample.php connection.php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+\Sentry\init([
+    'dsn' => 'https://0a4b9a9ba8d0bdf36d1c06376470d33b@o4511841982873600.ingest.de.sentry.io/4511842063941712',
+    'traces_sample_rate' => 1.0,
+    'enable_logs' => true,
+]);
+
 $host     = '127.0.0.1'; // адрес сервера
 $database = '';           // имя БД (например: provodnik)
 $user     = '';           // пользователь MySQL
